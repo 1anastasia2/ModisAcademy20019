4e5d1581dc6606686ac664935095dee9f7a46171
1anastasia2 <azia.bellissima@gmail.com>
2019-12-12 16:53:15 +0100

    metodo action after take a beer

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -1,37 +1,41 @@
 import java.io.PrintStream;
 
-class Bottles
-{
-    public static void main(String args[])
-    {
+class Bottles {
+    public static void main(String args[]) {
         writeLyrics(System.out);
     }
 
     public static void writeLyrics(PrintStream out) {
-        for (int beers=99; beers>-1;beers--)
-        {
-            if (beers==0)
-            {
+        for (int beers = 99; beers > -1; beers--) {
+            if (beers == 0) {
                 out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles"  + " of beer,");
-                out.print("Go to the store, buy some more,");
+                out.println(beers + " bottles" + " of beer,");
+                out.print(actionAfterTakeBeer(beers));
                 out.println("99 bottles of beer on the wall.\r\n");
-            } else if(beers ==1) {
+            } else if (beers == 1) {
                 out.print(beers + " bottle" + " of beer on the wall,");
                 out.println(beers + " bottle" + " of beer,");
-                out.print("Take one down, pass it around,");
-                out.println(beers -1 +  " bottles" +  " of beer on the wall.\r\n");
-            } else if(beers == 2) {
-                    out.print(beers + " bottles" + " of beer on the wall,");
-                    out.println(beers + " bottles"  + " of beer,");
-                    out.print("Take one down, pass it around,");
-                    out.println(beers-1 + " bottle" + " of beer on the wall.\r\n");
+                out.print(actionAfterTakeBeer(beers));
+                out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
+            } else if (beers == 2) {
+                out.print(beers + " bottles" + " of beer on the wall,");
+                out.println(beers + " bottles" + " of beer,");
+                out.print(actionAfterTakeBeer(beers));
+                out.println(beers - 1 + " bottle" + " of beer on the wall.\r\n");
             } else {
-                    out.print(beers + " bottles"  + " of beer on the wall,");
-                    out.println(beers + " bottles" + " of beer,");
-                    out.print("Take one down, pass it around,");
-                    out.println(beers-1 + " bottles" + " of beer on the wall.\r\n");
-                }
+                out.print(beers + " bottles" + " of beer on the wall,");
+                out.println(beers + " bottles" + " of beer,");
+                out.print(actionAfterTakeBeer(beers));
+                out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
+            }
+        }
+    }
+
+    private static String actionAfterTakeBeer(int beer) {
+        if (beer == 0) {
+            return "Go to the store, buy some more,";
+        } else {
+            return "Take one down, pass it around,";
         }
     }
 }
